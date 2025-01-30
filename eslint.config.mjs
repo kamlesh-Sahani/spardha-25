d425a69aa0de9a711dd32781a48c3c1a23aea58a
kamlesh <alex20150001969@gmail.com>
2025-01-30 20:49:17 +0530

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Add rules you want to disable here
+      "rule-name-to-disable": "off", // Replace with the actual rule name
+      "another-rule-to-disable": "off",   
+      "@typescript-eslint/no-unused-vars": "off", // Disables unused variable checks
+    "@typescript-eslint/no-explicit-any": "off" ,// Disables explicit any warnings    
+    "@typescript-eslint/no-empty-object-type": "off",  // Disable empty object type warning
+    "prefer-const": "off",
+
+    },
+  },
 ];
 
 export default eslintConfig;
