c3736e300114bb31322591cc893167b2e9044c6b
kamlesh <alex20150001969@gmail.com>
2025-01-31 17:06:22 +0530

    profile/layout

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import {useRouter} from "next/router";
 const layout = ({children}:{children:React.ReactNode}) => {
-  const router = useRouter();
+
   return (
     <>
      {children}
