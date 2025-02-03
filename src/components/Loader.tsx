0757d0a88dfbfd352835ec420dea1bd7b39d1fa8
kamlesh <alex20150001969@gmail.com>
2025-02-03 21:45:08 +0530

    changes

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,7 +4,7 @@ const Loader = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-transparent">
     <div className="flex flex-col items-center">
-      <div className="animate-spin rounded-full border-8 border-t-8 border-blue-500 w-20 h-20 mb-4"></div>
+      <div className="animate-spin rounded-full border-8 border-t-8 border-[#3D2F51] w-20 h-20 mb-4"></div>
       <h2 className="text-xl text-gray-700 font-semibold animate-fadeIn opacity-90">
         Loading...
       </h2>
