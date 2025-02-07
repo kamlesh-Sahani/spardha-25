3d7075a0a650c9ab3929c1aba16e6dc8c1da8fe8
kamlesh <alex20150001969@gmail.com>
2025-02-07 16:01:07 +0530

    es rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends(["next/core-web-vitals", "next/typescript"]),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
       // Add rules you want to disable here
